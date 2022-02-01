06ce519a13ad3c826d33ebc1826ea106c44a5974
imanmansour86 <imanmansour86@gmail.com>
2022-01-31 20:25:38 -0800

    remove express code from server.js

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ const express = require("express");
 const inquirer = require("inquirer");
 
 const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 const mainMenuQuestions = [
   {
@@ -39,12 +34,3 @@ function init() {
   );
   mainMenu();
 }
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
